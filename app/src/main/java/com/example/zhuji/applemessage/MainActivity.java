288525e692a53299d20832bf7d37e03400f38b3f
zhujianjie1989 <zhujianjie_1989@163.com>
2015-09-17 12:27:01 +0800

    creat

---
+++ app/src/main/java/com/example/zhuji/applemessage/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.zhuji.applemessage;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void send2(String number, String message){
+        String SENT = "sms_sent";
+        String DELIVERED = "sms_delivered";
+
+        PendingIntent sentPI = PendingIntent.getActivity(this, 0, new Intent(SENT), 0);
+        PendingIntent deliveredPI = PendingIntent.getActivity(this, 0, new Intent(DELIVERED), 0);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                switch(getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Log.i("====>", "Activity.RESULT_OK");
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Log.i("====>", "RESULT_ERROR_GENERIC_FAILURE");
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Log.i("====>", "RESULT_ERROR_NO_SERVICE");
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Log.i("====>", "RESULT_ERROR_NULL_PDU");
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Log.i("====>", "RESULT_ERROR_RADIO_OFF");
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                switch(getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Log.i("====>", "RESULT_OK");
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i("=====>", "RESULT_CANCELED");
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager smsm = SmsManager.getDefault();
+        smsm.sendTextMessage(number, null, message, sentPI, deliveredPI);
+    }
+}
